0a8bbda9bae4dad0abc3e459210da89fb3e981a9
DF-81 <66225262+DF-81@users.noreply.github.com>
2026-06-16 11:17:58 +0200

    Update skills to include Python in profile

--- data/profile.js
+++ data/profile.js
@@ -32,7 +32,7 @@ const profileData = {
   skills: [
     {
       title: "Sprachen & Standards",
-      value: "HTML, CSS, PHP, SQL, JavaScript, JSON",
+      value: "Python, HTML, CSS, PHP, SQL, JavaScript, JSON",
     },
     {
       title: "Werkzeuge & Plattformen",
